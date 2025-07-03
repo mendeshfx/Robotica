260d7e9653f79a16879602eee5d3ba26f89f3d63
mendeshfx <megalolo19@gmail.com>
2025-07-03 08:49:08 -0300

    Update Questão 5.cpp

--- Questão 5.cpp
+++ Questão 5.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    std::string nome;
+    int idade;
+    
+    cout<<"Informe o seu nome: ";
+    cin>> nome ;
+    cout<<"Informe sua idade: ";
+    cin>> idade;
+    
+    cout<< nome<<" tem "<<idade<<" anos de idade."<<endl;
+
+    return 0;
+}
+
 
